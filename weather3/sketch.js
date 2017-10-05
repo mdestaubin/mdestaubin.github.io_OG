f4c7eaf33046a5a9ef86fab183869ea210cf46ab
mdestaubin <mdestaubin@gsd.harvard.edu>
2017-10-05 09:19:49 -0400

    update

--- weather3/sketch.js
+++ weather3/sketch.js
@@ -4,7 +4,7 @@ var queryResult;
 
 function setup() 
 {
-  pixelDensity(3.0);
+  //pixelDensity(3.0);
   createCanvas(375,667);
   background(255);
  
@@ -36,17 +36,22 @@ function gotData(data)
 
   
   // a few variables for text formatting
-  var xPos1 = 20; 
-  var xPos2 = width/2
-  var xPos3 = width/2 
+  var xPos1 = (width/2)-110; 
+  var xPos2 = width/2;
+  var xPos3 = (width/2)+110;
+  var yPos1 = 270;
+  var yPos2 = 380;
+  var yPos3 = 490;
+  var yPos4 = 600;
+
   var yPos = 40;
   var yGap = 60; 
   var textSizeLarge = 20;
   var textSizeSmall = 16;
   var textSizeDegrees = 30;
 
-  var iconRadiusBig = 100;
-  var iconRadiusSmall = 40;
+  var iconRadiusBig = 120;
+  var iconRadiusSmall = 50;
 
   var m = month();
   var d = day();
@@ -65,33 +70,41 @@ function gotData(data)
 
  
   // Current Weather
- 
-   fill(255,50);
-   ellipse(xPos2,130,iconRadiusBig,iconRadiusBig);
+   noStroke();
+   fill(50,50,200,50);
+   ellipse(xPos2,140,iconRadiusBig,iconRadiusBig);
+
+   fill(0);
+   textSize(textSizeDegrees);
+   textAlign(CENTER);
+   text(Math.round(hourlyWeather.data[0].temperature), xPos2,150);
 
  
   // future weather ellipses
+  fill(50);
+  ellipse(xPos1,yPos1,iconRadiusSmall,iconRadiusSmall);
+  ellipse(xPos2,yPos1,iconRadiusSmall,iconRadiusSmall);
+  ellipse(xPos3,yPos1,iconRadiusSmall,iconRadiusSmall);
 
-  ellipse(xPos2,300,iconRadiusSmall,iconRadiusSmall);
-  ellipse(80,455,80,80);
+  ellipse(xPos1,yPos2,iconRadiusSmall,iconRadiusSmall);
+  ellipse(xPos2,yPos2,iconRadiusSmall,iconRadiusSmall);
+  ellipse(xPos3,yPos2,iconRadiusSmall,iconRadiusSmall);
 
+  ellipse(xPos1,yPos3,iconRadiusSmall,iconRadiusSmall);
+  ellipse(xPos2,yPos3,iconRadiusSmall,iconRadiusSmall);
+  ellipse(xPos3,yPos3,iconRadiusSmall,iconRadiusSmall);
+
+  ellipse(xPos1,yPos4,iconRadiusSmall,iconRadiusSmall);
+  ellipse(xPos2,yPos4,iconRadiusSmall,iconRadiusSmall);
+  ellipse(xPos3,yPos4,iconRadiusSmall,iconRadiusSmall);
   
-  // Time Text
-  
-  fill(0);
-  textSize(textSizeDegrees);
-  textAlign(CENTER);
-  text(Math.round(hourlyWeather.data[0].temperature), xPos2,140);
-
 
   // settings and arrow
-  noFill();
+
   strokeWeight(3);
   stroke(0);
-  ellipseMode(CENTER);
-  ellipse(20,549,14,14);
-  line(width-25,556,width-15,549);
-  line(width-25,542,width-15,549);
+  line(width-25,(height-20)+5,width-15,(height-20));
+  line(width-25,(height-20)-5,width-15,(height-20));
 
 
   // border outline
