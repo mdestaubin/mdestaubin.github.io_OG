d8658611a48aa9e8160b93a1b61dccacfa268a33
mdestaubin <31933880+mdestaubin@users.noreply.github.com>
2017-12-07 23:47:08 -0500

    Add files via upload

--- ebolaAid/sketch.js
+++ ebolaAid/sketch.js
@@ -1,5 +1,6 @@
 // this table will contain the numbers and actual data
 var table;
+var table2;
 
 var margin = 1150;
 var title  = 42;
@@ -16,14 +17,16 @@ var yHealth = 615;
 
 // this is an array that will contain all the data in form of javascript objects
 var states = [];
+var states2 = [];
 var leafletMap;
 var worldJSON;
+var path;
 
 var m; 
 var r;
 
-
 var slider;
+var sliderValue;
 
 
 function preload() {
@@ -31,6 +34,7 @@ function preload() {
   //I'm ignoring the header 
 
   table       = loadTable("data/newData.csv", "csv");
+  table2      = loadTable("data/testData.csv", "csv");
   myFontThin  = loadFont('Text/frutiger-thin.otf');
   myFontBlack = loadFont('Text/frutiger-black.otf');
 }
@@ -40,26 +44,23 @@ function setup() {
 
    var canvas = createCanvas(1600,1475);  
    canvas.parent("container");
-   parseData();  
+   parseData();
+   parseData2();  
    createMap();
    textFont(myFontThin);
 
 
+   var numRows = table.getRowCount(); 
 
-  slider = createSlider(0, 144, 72);
+   console.log("rows = " + numRows);
+
+  slider = createSlider(0, numRows, 1);
   slider.position(57, 1450);
   slider.style('width', '1525px');
-
- // slider.style('color', 'white');
-
-
- }
-
-
-function draw(){
-
-   background(255);
-   textFont(myFontThin);
+  slider.style('height', '2px');
+  //slider.style('background', 'transparent');
+  //slider.style('border', 'white');
+     textFont(myFontThin);
 
    noStroke();
    textSize(40);
@@ -67,44 +68,55 @@ function draw(){
    text("EBOLA  AID  FLOW  MAP",57, 55);
    text("AID DESTINATIONS",57, 790);
    
-   
-   // textFont(myFontBlack);
-   // textSize(40);
-   // fill('#3a913b');
-   // text("2014 WEST AFRICA EBOLA OUTBREAK",900, 60);
-   
-   textSize(26);
-   text("Click a Country",122, 635);
+    textSize(16);
+    textAlign(CENTER);
+    fill(58, 145, 59,200);
+    var xPos2 = 100;
+    var yPos2 = 900;
+    var yPos3 = 1200;
+    ellipse(xPos2,yPos2,map(238168759, 0, 1262994929, 0, 400),map(238168759, 0, 1262994929, 0, 400));
+    text("National Government",xPos2,yPos2+map(238168759, 0, 1262994929, 0, 400)/2 +20);
+    xPos2 += 200;
+    ellipse(xPos2,yPos3,map(1028025604, 0, 1262994929, 0, 400),map(1028025604, 0, 1262994929, 0, 400));
+    text("NGO",xPos2,yPos3+map(1028025604, 0, 1262994929, 0, 400)/2 +20);
+    xPos2 += 200;
+    ellipse(xPos2,yPos2,map(19415212, 0, 1262994929, 0, 400),map(19415212, 0, 1262994929, 0, 400));
+    text("Inter-Governmental",xPos2,yPos2+map(19415212, 0, 1262994929, 0, 400)/2 +20);
+    xPos2 += 200;
+    ellipse(xPos2 + 60,yPos3,map(753460487, 0, 1262994929, 0, 400),map(753460487, 0, 1262994929, 0, 400));
+    text("Not Specified",xPos2+ 60,yPos3+map(753460487, 0, 1262994929, 0, 400)/2 +20);
+    xPos2 += 200;
+    ellipse(xPos2,yPos2,map(103121737, 0, 1262994929, 0, 400),map(103121737, 0, 1262994929, 0, 400));
+    text("Private Organization",xPos2,yPos2+map(103121737, 0, 1262994929, 0, 400)/2 +20);
+    xPos2 += 200;
+    ellipse(xPos2,yPos2,map(153964272, 0, 1262994929, 0, 400),map(153964272, 0, 1262994929, 0, 400));
+    text("Red Cross/Red Crescent",xPos2,yPos2+map(153964272, 0, 1262994929, 0, 400)/2 +20);
+    xPos2 += 200;
+    ellipse(xPos2,yPos3,map(1262994929, 0, 1262994929, 0, 400),map(1262994929, 0, 1262994929, 0, 400));
+    text("UN Agency",xPos2,yPos3+map(1262994929, 0, 1262994929, 0, 400)/2 +20);
+    xPos2 += 200;
+    ellipse(xPos2,yPos2,map(58802154, 0, 1262994929, 0, 400),map(58802154, 0, 1262994929, 0,400));
+    text("Multiple Organization Types",xPos2,yPos2+map(58802154, 0, 1262994929, 0, 400)/2 +20);
+     
 
+   noFill();
+   var rX = width-225;
+   var rY = 325;
+   var h = 400;
+   stroke('#3a913b');
+   strokeWeight(.5);
+   ellipse(rX,rY,h,h);
+   noStroke();
+   fill('#3a913b');
+   text("COUNTRY DATA", rX, rY);
+   textAlign(LEFT);
 
-   fill(255,0,0);
-     // var val = float(slider.value());
-   text("slider value = " + slider.value(),80, 505);
-
-
-   //rect(40,40,100,100);
-
-
- //   strokeWeight(51);
- //   line(112,610,800,610);
-
- //   if(mouseY > 580 && mouseY < 620){
- //   if (mouseIsPressed) 
- //   ellipse(mouseX,610,10,10);
- // }
- //   else  {
- //   ellipse(125,370,10,10);
- // }
-
-   activeGraph();
-   //scrollBar();
-
-}
-
+ }
 
 // create the map using leaflet
 
 function createMap(){
+  background(255);
   //var state = findStateByName(admin);
 
   leafletMap = L.map('map').setView([22, 0], 2);
@@ -123,6 +135,37 @@ function createMap(){
 
 });
 
+  // var pointA = L.LatLng(45,28.520507812500004);
+  // var pointB = L.LatLng(10, -10);
+
+  // var path = L.curve(['M',[45,28.520507812500004],
+  //         'C',[80,0],
+  //           [60,-10],
+  //           [10,-10]],
+  //         {
+  //         color:'green',
+  //         fill:false,
+
+  //       }).addTo(leafletMap);
+
+  //      path2 = L.curve(['M',[45,-90],
+  //         'C',[100,-20],
+  //           [60,-10],
+  //           [10,-10]],
+  //         {color:'green',fill:false}).addTo(leafletMap);
+
+//   var arcedPolyline = L.ArcedPolyline([45,28.520507812500004], {
+//   color: '#FF00000',
+//   weight: 4
+// }).addTo(leafletMap);
+
+// var arcedPolyline = L.ArcedPolyline([45,28.520507812500004, 0, 0], {
+//   distanceToHeight: L.LinearFunction([0, 0], [4000, 400]),
+//   color: '#FF00000',
+//   weight: 4,
+// });
+// leafletMap.addLayer(arcedPolyline);
+
   ////////////////////////////////////////////////////// World Map JSON
 
   worldJSON = L.geoJson(worldMap, {
@@ -140,18 +183,13 @@ function style(feature) {
       //  console.log(stateNow);
       var m = 0;
      if(stateNow!=undefined){ 
-       var paid = float(stateNow.occupations[0].value);
-       m = map(paid, 0, 150000000,.1,.6);
-
+       var paid = float(stateNow.occupations[1].value);
+       m = map(paid, 0, 50000000,.2,.8);
      }
 //     console.log(m);
 
- // var m = map(state.occupations[1].value,0,359540696,0,1);
-
- // var m = map(state.occupations[1].value,0,359540696,0,1);
-
   return {
-    weight: 1,
+    weight: .5,
     opacity: .5,
     color: '#58a52e',
     dashArray: '1',
@@ -198,28 +236,9 @@ function activateGraph(e) {
 
   var countryName = layer.feature.properties.admin;
   var region = layer.feature.properties.region_wb;
-  
-  //var center = layer.feature.geometry.coordinates.getCenter();
-
-   fill(255);
-   noStroke(0);
-   rect(118,488,500,160);
-   noStroke();
-   textFont(myFontThin);
-   textSize(26);
-   fill('#3a913b');
-   textAlign(LEFT);
-   // text(countryName,122, 635);
-   // textFont(myFontThin);
-   // textSize(16);
-   // text(region, 122, 470);
-   // text("Pledged: XXX", 122, 500);
-   // text("Paid Contribution: XXX", 122, 525);
-   // text("Total Aid: XXX", 122, 550);
-  
 
      console.log(countryName);
-     //createGraph(layer.feature.properties.admin);
+    // createGraph(layer.feature.properties.admin);
      playTimeline(layer.feature.properties.admin);
 
  }
@@ -230,8 +249,9 @@ function parseData(){
    var keyRow = table.getRow(0);
    var metaRow = table.getRow(1);
 
+
     // cycle through each item in that column, ignoring the first two items which are the headers
-    for(var i=2;i<table.getRowCount(); i++){    
+    for(var i=2;i<table.getRowCount(); i++){   
 
       //get each row for each id, hence the data for one state at a time
       var stateRow = table.getRow(i);
@@ -262,94 +282,42 @@ function parseData(){
 
 }
 
+function parseData2(){
+   //this is the key at the top, we will need it later
+   var keyRow = table2.getRow(0);
+   var metaRow = table2.getRow(1);
 
+    // cycle through each item in that column, ignoring the first two items which are the headers
+    for(var i=2;i<table2.getRowCount(); i++){    
 
-//  function createGraph(admin){
-  
-//   //var state = findStateByName(admin);
-//   console.log(admin);
+      //get each row for each id, hence the data for one state at a time
+      var stateRow = table2.getRow(i);
 
-//   var runningTotal;
-//   var state;
-//   for(var i=0; i<states.length; i++){
-//     if(states[i].name == admin){
-//       state = states[i];
-//     }
-//   }
-  
-// //   // // first let's find the highest value for this state
-//   var maxValue =200000000;
+      // create an empty object for each state
+      // we will attach all the information to this object
+      var state2 = {};
+      state2.id = stateRow.getString(0);
+      state2.id2 = stateRow.getString(1);
+      state2.name = stateRow.getString(2);
 
-//   // for(var i=1; i<country.values.length; i++){
-//   //   if(country.values[i].value>maxValue)
-//   //     maxValue = country.values[i].value;
-//   // }
+      // this array will hold all occupation data
+      state2.occupations = [];
+      
+      for(var j=3; j<table.getColumnCount(); j++){
+        // create an empty object that holds the occupation data for one category
+        var item2 = {};
+        item2.label = metaRow.getString(j);
+        item2.key = keyRow.getString(j);
+        item2.value = stateRow.getString(j);
+        
+        // attach the item object to the "occupation" array
+        append(state2.occupations, item2);
+      }
+      // attach the state object to the "states" array
+      append(states2, state2);
+   }
 
-//   console.log("maxValue: " + maxValue);
-
-//   fill(255);
-//   rect(width-550,0,width/2,height);
-//   fill('#3a913b');
-
-//   var xPos = 100;
-//   var yPos = 1600;
-
-//   //go through all states
-//   // for(var i =0; i<70; i++){
-//   //   // Display the state name
-//   //   var stateDiv = createDiv(states[i].name);
-//   //   stateDiv.position(xPos, yPos);
-//   //   stateDiv.style("font-size", "50%");
-//   //   stateDiv.style("color", "#c6c6c6");
-
-//   //   // Display the population count (the first name in the array)
-//   //   // var popDiv = createDiv(states[i].occupations[0].value);
-//   //   // popDiv.position(xPos + 200, yPos);
-//   //   // popDiv.style("width", "100px");
-//   //   // popDiv.style("text-align", "right")
-//   //   yPos +=8;
-
-//   //   var rectX = map(states[i].occupations[0].value,0, 300000000, 1, 400);
-//   //   rect(100,yPos,rectX,5);
-
-    
-//   // }
-
-
-//     if(float(state.occupations[1].value) >= 200000000){
-//        var h = 500;
-//     }
-//     else {
-//       var h = map(float(state.occupations[1].value),0, maxValue, 3, 1000);
-//     }
-
-//     var rX = width-235;
-//     var rY = 325;
-    
-//     // fill(0,255,0,100);
-//     // triangle(mouseX,mouseY,rX,rY+h/2,rX,rY-h/2);
-//     fill('#3a913b');
-//     ellipse(rX,rY,h, h);
-//    //  //stroke('#3a913b');
-//    //  //line(mouseX,mouseY,rX,rY);
-//    // // noStroke();
-   
-//    var str = map(float(state.occupations[1].value),0, 100000000, 1, 8);
-//    strokeWeight(str);
-//    stroke(58, 145, 59,180);
-//    noFill();
-//    bezier(mouseX,mouseY, 500, -200, 550, 525, 547,417);
-//    noStroke();
-   
-//     fill('#3a913b');
-//     textSize(26);
-//     textAlign(CENTER);
-//     var paid = nfc(float(state.occupations[1].value));
-//     text("Paid Contribution: $" + paid,rX,rY + h/2 + 30);
-
-
-
-//  }
+}
 
   function playTimeline(admin){
   
@@ -362,6 +330,7 @@ function parseData(){
   var tempY = mouseY;
   var paidList = [];
   var count = 0;
+  var maxTime = slider.value();
 
 
   for(var i=0; i<states.length; i++){
@@ -373,50 +342,34 @@ function parseData(){
 
     }
   }
-
-  for(var i=0; i<states.length; i++){
+   
+  for(var i=0; i<maxTime; i++){
     if(states[i].name == admin){
-      state = states[i];
+      var state = states[i];
       var paidDollars = float(state.occupations[1].value);
       runningTotal += paidDollars;
+
       console.log(runningTotal);
+
+//}
+      // if (sliderValue > date) {
+      //       break;
+      //    }
+
   
 //   // // first let's find the highest value for this state
   var maxValue =200000000;
 
   fill(255);
-  rect(width-550,50,width/2,height);
+  rect(width-550,50,width/2,height/2);
   fill('#3a913b');
-
-  textSize(26);
-  text("Click a Country",122, 635);
 
   var xPos = 100;
   var yPos = 1000;
 
-  // //go through all states
-  // for(var i=0; i<states.length; i++){
-  //   // Display the state name
-  //   var stateDiv = createDiv(states[i].name);
-  //   stateDiv.position(xPos, yPos);
-  //   stateDiv.style("font-size", "50%");
-  //   stateDiv.style("color", "#c6c6c6");
-
-  //   // Display the population count (the first name in the array)
-  //   // var popDiv = createDiv(states[i].occupations[0].value);
-  //   // popDiv.position(xPos + 200, yPos);
-  //   // popDiv.style("width", "100px");
-  //   // popDiv.style("text-align", "right")
-  //   yPos +=8;
     
-   
-  // //  var rectX = map(runningTotal,0, maxValue, 1, 400);
-  // //  rect(100,yPos,rectX,5);
-  //    }
-    
-
     if(runningTotal >= 200000000){
-       var h = 300;
+       var h = 400;
     }
     else {
       var h = map(runningTotal,0, 200000000, 3, 600);
@@ -435,7 +388,7 @@ function parseData(){
    
    var str = map(runningTotal,0, 100000000, 1, 7);
    strokeWeight(str);
-   stroke(58, 145, 59,40);
+   stroke(58, 145, 59,100);
    noFill();
    bezier(tempX,tempY, 500, -200, 550, 525, 547,417);
    noStroke();
@@ -447,46 +400,17 @@ function parseData(){
     text("Paid Contribution: $" + paid,rX,rY + h/2 + 90);
     text(states[i].name,rX,rY + h/2 + 60);
     textAlign(LEFT);
-
-    fill(255,0,0);
-     // var val = float(slider.value());
-    text("slider value = " + slider.value(),80, 505);
-
-    var countValue = count;
-    text("total payment counts = " + countValue, 80, 535);
-    
-    fill('#3a913b');
-    var xPos2 = 80;
-    var yPos2 = 830;
-    rect(xPos2,yPos2,100,map(238168759, 0, 1262994929, 0, 600));
-    xPos2 += 200;
-    rect(xPos2,yPos2,100,map(19415212, 0, 1262994929, 0, 600));
-    xPos2 += 200;
-    rect(xPos2,yPos2,100,map(1028025604, 0, 1262994929, 0, 600));
-    xPos2 += 200;
-    rect(xPos2,yPos2,100,map(753460487, 0, 1262994929, 0, 600));
-    xPos2 += 200;
-    rect(xPos2,yPos2,100,map(103121737, 0, 1262994929, 0, 600));
-    xPos2 += 200;
-    rect(xPos2,yPos2,100,map(153964272, 0, 1262994929, 0, 600));
-    xPos2 += 200;
-    rect(xPos2,yPos2,100,map(1262994929, 0, 1262994929, 0, 600));
-    xPos2 += 200;
-    rect(xPos2,yPos2,100,map(58802154, 0, 1262994929, 0, 600));
   }
  }
-
-
-
- }
+}
 
 // helper function to find a state by name
 function findStateByName(admin){
-  var state;
-  for(var i=0; i<states.length; i++){
-    if(states[i].name == admin){
-      state = states[i];
-      return state;
+  var state2;
+  for(var i=0; i<states2.length; i++){
+    if(states2[i].name == admin){
+      state2 = states2[i];
+      return state2;
     }
   }
 }
