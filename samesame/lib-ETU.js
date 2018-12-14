f56534a7c8ba7556dca29c7652744bfa6dc39384
mdestaubin <31933880+mdestaubin@users.noreply.github.com>
2018-12-14 17:59:48 -0500

    Add files via upload

--- samesame/lib-ETU.js
+++ samesame/lib-ETU.js
@@ -36,6 +36,23 @@ var libETUData = {
 		}
 	}, 
 
+	{
+		
+		"type": "Feature",
+		"id": "lbr_heal_pt_unmeer_ebolacarefacilities.1",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-71.121034, 42.380500]
+		},
+		"geometry_name": "the_geom",
+		"properties": {
+			"Type": "https://github.com/mdestaubin/mdestaubin.github.io/blob/master/samesame/images/35%20langdon%20st.png?raw=true",
+			"Beds_Plan": '</br><b>A.</b></br>35 Langdon Street',
+			"Lead_Donor": "https://mdestaubin.github.io/portfolio/links/rehab/index",
+			"Updated": "2018-12-14T00:00:00Z"
+		}
+	}, 
+
 	],
 
 	"crs": {
