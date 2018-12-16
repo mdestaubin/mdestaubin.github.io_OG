e667f85b31856c45ec10eca0ee0ae6d857d0dc36
mdestaubin <31933880+mdestaubin@users.noreply.github.com>
2018-12-16 13:57:17 -0500

    Add files via upload

--- samesame/lib-ETU.js
+++ samesame/lib-ETU.js
@@ -12,7 +12,8 @@ var libETUData = {
 		"geometry_name": "the_geom",
 		"properties": {
 			"Type": "https://raw.githubusercontent.com/mdestaubin/mdestaubin.github.io/master/samesame/images/353%20harvard%20st.png",
-			"Beds_Plan": '</br><b>A.</b></br>353 Harvard Street',
+			"Beds_Plan": '</br><b>A.</b></br>353 Harvard Street</br>Cambridge, MA 02138',
+			"Class": 'a',
 			"Partner": "https://raw.githubusercontent.com/mdestaubin/mdestaubin.github.io/master/samesame/images/353%20harvard%20st.png",
 			"Lead_Donor": "https://mdestaubin.github.io/portfolio/links/rehab/index",
 			"Updated": "2018-12-14T00:00:00Z"
@@ -30,7 +31,8 @@ var libETUData = {
 		"geometry_name": "the_geom",
 		"properties": {
 			"Type": "https://raw.githubusercontent.com/mdestaubin/mdestaubin.github.io/master/samesame/images/18%20skehan%20st.png",
-			"Beds_Plan": '</br><b>B.</b></br>18 Skehan Street',
+			"Beds_Plan": '</br><b>B.</b></br>18 Skehan Street</br>Cambridge, MA 02138',
+			"Class": 'b',
 			"Lead_Donor": "https://mdestaubin.github.io/portfolio/links/rehab/index",
 			"Updated": "2018-12-14T00:00:00Z"
 		}
@@ -47,9 +49,46 @@ var libETUData = {
 		"geometry_name": "the_geom",
 		"properties": {
 			"Type": "https://github.com/mdestaubin/mdestaubin.github.io/blob/master/samesame/images/35%20langdon%20st.png?raw=true",
-			"Beds_Plan": '</br><b>A.</b></br>35 Langdon Street',
+			"Beds_Plan": '</br><b>A.</b></br>35 Langdon Street</br>Cambridge, MA 02138',
+			"Class": 'a',
 			"Lead_Donor": "https://mdestaubin.github.io/portfolio/links/rehab/index",
 			"Updated": "2018-12-14T00:00:00Z"
+		}
+	}, 
+
+	{
+		
+		"type": "Feature",
+		"id": "lbr_heal_pt_unmeer_ebolacarefacilities.1",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-71.105631, 42.376759]
+		},
+		"geometry_name": "the_geom",
+		"properties": {
+			"Type": "https://github.com/mdestaubin/mdestaubin.github.io/blob/master/samesame/images/50%20Line%20Street.png?raw=true",
+			"Beds_Plan": '</br><b>Tripple Decker</b></br>50 Line Street</br>Somerville, MA 02143',
+			"Class": 'c',
+			"Lead_Donor": "https://mdestaubin.github.io/portfolio/links/rehab/index",
+			"Updated": "2018-12-16T00:00:00Z"
+		}
+	}, 
+
+	{
+		
+		"type": "Feature",
+		"id": "lbr_heal_pt_unmeer_ebolacarefacilities.1",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [-71.104336, 42.361581]
+		},
+		"geometry_name": "the_geom",
+		"properties": {
+			"Type": "https://github.com/mdestaubin/mdestaubin.github.io/blob/master/samesame/images/rollins%20court.png?raw=true",
+			"Beds_Plan": '</br><b>Townhomes</b></br>Rollins Court</br>Cambridge, MA 02139',
+			"Class": 'd',
+			"Lead_Donor": "https://mdestaubin.github.io/portfolio/links/rehab/index",
+			"Updated": "2018-12-16T00:00:00Z"
 		}
 	}, 
 
--- samesame/sketch.js
+++ samesame/sketch.js
@@ -13,6 +13,9 @@ var path;
 var libJSON;
 var marker;
 
+var cambuild;
+var mapLayerGroups = [];
+
 
 function setup() {  
    background(0);
@@ -20,7 +23,6 @@ function setup() {
    canvas.parent("container"); 
    createMap();
    textFont(myFontThin);
-    
  }
 
 // create the map using leaflet
@@ -55,14 +57,73 @@ function createMap(){
       style: ETUstyle,
       pointToLayer : pointToLayer,
       onEachFeature: onEachETU
+// }); 
 }).addTo(leafletMap); 
+
+  //  cambuild = L.geoJson(cambuild, {
+  //  style: style,
+  //  onEachFeature: onEachFeature
+  // }).addTo(leafletMap);  
 
   }
 
+// showLayer("a");
+
+// function showLayer(id) {
+//     var lg = mapLayerGroups[id];
+//     map.addLayer(lg);   
+// }
+
+// function hideLayer(id) {
+//     var lg = mapLayerGroups[id];
+//     map.removeLayer(lg);   
+// }
+
 /////////////////////////////////////////////////// Country Settings
 
- function onEachETU(feature, layer) {
-     layer.bindPopup( 
+ function getColor(d) {
+  switch (d) {
+    case 'a':
+      return '#540D6E';
+    case 'b':
+      return '#EE4266';
+    case 'c':
+      return '#3BCEAC';
+    case 'd':
+      return '#0EAD69';
+    case 'e':
+      return '#FFD23F';
+    default:
+      return 'black';
+  }
+}
+
+ function style(feature) {
+
+  return {
+    weight: .5,
+    opacity: .75,
+    color: '#000000',
+    fillOpacity: '1',
+    fillColor: 'blue',
+    background: 'blue'
+  };
+}
+
+ function onEachETU(feature, layer, featureLayer) {
+    // var lg = mapLayerGroups[feature.properties.Class];
+
+    // if (lg === undefined) {
+    //     lg = new L.layerGroup();
+    //     //add the layer to the map
+    //     lg.addTo(leafletMap);
+    //     //store layer
+    //     mapLayerGroups[feature.properties.Class] = lg;
+    // }
+
+    // lg.addLayer(featureLayer); 
+
+    layer.bindPopup( 
       "<img src=" + feature.properties.Type + " width = '200px'/>" + "<br>" + 
        feature.properties.Beds_Plan 
           );
@@ -83,9 +144,9 @@ function pointToLayer(feature, latlng) {
 
 function ETUstyle(feature) {
   return {      
-      radius: 1.0, 
+      radius: 1.5, 
       weight: 3.0, 
-      color: 'red', 
+      color: getColor(feature.properties.Class), 
       fillOpacity: 0};
       }
 
@@ -95,7 +156,7 @@ function highlightETU(e) {
     layer.setStyle({
     radius: 7,
     weight: 2,
-    color: 'red',
+    color: getColor(feature.properties.Class),
     fillOpacity: .4,
 
   });
